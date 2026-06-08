74bb2e084662e8729bf885839581394b4d8cd8f8
johnsonyuen <j@johnsonyuen.com>
2026-06-08 19:41:41 +1000

    ci: use mirrored postgres image for python tests

---
+++ server/tests/test_testcontainers_config.py
@@ -0,0 +1,13 @@
+from tests.conftest import postgres_test_image
+
+
+def test_postgres_test_image_defaults_to_docker_hub(monkeypatch):
+    monkeypatch.delenv("POSTGRES_TEST_IMAGE", raising=False)
+
+    assert postgres_test_image() == "postgres:16"
+
+
+def test_postgres_test_image_can_be_overridden(monkeypatch):
+    monkeypatch.setenv("POSTGRES_TEST_IMAGE", "public.ecr.aws/docker/library/postgres:16")
+
+    assert postgres_test_image() == "public.ecr.aws/docker/library/postgres:16"
